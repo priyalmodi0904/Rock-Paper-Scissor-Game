eb29a284d7cf117695fdd83802e3e9d6512db3e4
Priyal Modi <164036980+priyalmodi0904@users.noreply.github.com>
2024-12-15 01:40:30 +0530

    Update rock.js

--- rock.js
+++ rock.js
@@ -1,9 +1,11 @@
 let userScore=0;
 let compScore=0;
+
 const choice=document.querySelectorAll(".choice");
 const msg=document.querySelector("#msg");
 const userScorepara=document.querySelector("#user-score");
 const compScorepara=document.querySelector("#comp-score");
+
 const showWinner=(userWin,userChoice,compChoice)=>{
     if(userWin){
         userScore++;
@@ -24,29 +26,64 @@ const drawGame=()=>{
     console.log("game drawn");
     msg.innerText="Draw"
     msg.style.backgroundColor="#081b31"
+
 }
+
+
+
+
+
 const genComChoice=()=>{
     const options=["rock","paper","scissor"];
     const random=Math.floor(Math.random()*3);
     return options[random];
 };
+
+
 const playGame=(userChoice)=>{
     console.log("user choice=",userChoice);
     const compChoice=genComChoice();
     console.log("compChoice=",compChoice);
+
     if(userChoice===compChoice){
         drawGame();
+
     }
     else{
         let userWin=true;
         if(userChoice==="rock"){
             userWin=compChoice==="paper"?false:true;
+
         }else if(userChoice==="paper"){
             userWin=compChoice==="scissor"?false:true;
         }
         
         else {
            userWin= compChoice==="rock"?false:true;
+
         }
         showWinner(userWin,userChoice,compChoice);
 }
+
+
+
+
+
+
+
+};
+
+
+
+
+
+ choice.forEach((choice) =>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("id");
+        console.log("choice was clicked",userChoice);
+        playGame(userChoice);
+
+    });
+
+ 
+ });
